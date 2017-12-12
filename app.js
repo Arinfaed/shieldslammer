775b8376482d365b586a5afd1f7bcab4c8a70a76
Arinfaed <arinfaed@barad-dur.net>
2017-12-12 20:00:20 +0100

    Added some features
    - now only reacts to messages for certain guilds => role combinations
    - switched from bulkDelete to single delete because of the 2 week limit
    - inform the command issuer about the amount of messages to be deleted and remove that information afterwards

--- app.js
+++ app.js
@@ -12,18 +12,18 @@ const config = require("./config.json");
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  client.user.setGame(`on ${client.guilds.size} servers`);
+  client.user.setGame(`Final Fantasy 6`);
 });
 
 client.on("guildCreate", guild => {
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setGame(`on ${client.guilds.size} servers`);
+  client.user.setGame(`Final Fantasy 6`);
 });
 
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setGame(`the Soundtrack of FF6`);
+  client.user.setGame(`Final Fantasy 6`);
 });
 
 
@@ -31,12 +31,16 @@ client.on("message", async message => {
 
   if(message.author.bot) return;
   if(message.content.indexOf(config.prefix) !== 0) return;
-  
+  if(!Object.keys(config.observe).includes(message.guild.id)) return;
   
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
     
   if(command === "shieldslam") {
+
+    if(!message.member.roles.some(r=>config.observe[message.guild.id].includes(r.id)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+
     // This command removes all messages from all users in the channel, up to 100.
     
     // get the delete count, as an actual number.
@@ -48,11 +52,18 @@ client.on("message", async message => {
     
     // So we get our messages, and delete them. Simple enough, right?
     const fetched = await message.channel.fetchMessages({count: deleteCount});
+    // filter out the pinned messages, those are probably somehow important, arent they?
     const nonPinned = fetched.filter((value, key, collection) => {
       return !value.pinned;
     });
-    message.channel.bulkDelete(nonPinned)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+    const reply = message.reply(`Deleting ${nonPinned.array().length} Messages`);
+    //message.channel.bulkDelete(nonPinned)
+    //  .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+    nonPinned.forEach((value) => {
+        value.delete().catch(console.error);
+    });
+
+    reply.then((message) => { message.delete(); });
   }
 });
 
